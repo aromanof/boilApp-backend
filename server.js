44efb3d6b7af80fd03c783044bd68ba5e343d2d4
a.romanov <a.romanov@concert.ua>
2019-05-15 10:58:09 +0300

    mlab used for mongo db

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const bodyParser = require('body-parser');
 const saltRounds = 10;
 const app = express();
 let users;
+const uri = "mongodb://AlexAdmin:AlexAdmin1@ds155916.mlab.com:55916/boil_app";
+let db = mongojs(uri, ['ba_users']);
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://michaelel1411:Misha2661748@ds155916.mlab.com:55916/boil_app";
-const client = new MongoClient( uri, { useNewUrlParser: true });
-client.connect((err, dbclient) => {
-    console.log('DB Connected...');
-    console.log(dbclient);
-    if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-    }
-    console.log('DB Connected...');
-    users = dbclient.db("boilerAppDB").collection("ba_users");
-    console.log(users);
-    // perform actions on the collection object
-    client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+//     console.log('DB Connected...');
+//     console.log(client);
+//     if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//     }
+//     console.log('DB Connected...');
+//     users = client.db("boilerAppDB").collection("ba_users");
+//     console.log(users);
+//     // perform actions on the collection object
+//     client.close();
+// });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -36,17 +36,19 @@ app.route('/api/login').get((req, res) => {
 
 app.route('/api/login').post((req, res) => {
     console.log('inside login post');
-    if(!req.body) return res.sendStatus(400);
-    res.send(`password checked`);
+    db.ba_users.find( (err, tasks) => res.send(tasks));
+    // if(!req.body) return res.sendStatus(400);
+    // res.send(`password checked`);
 });
 
 app.post('/login', bodyParser.json(), (req, res) => {
     console.log('inside login post');
-    if(!req.body) return res.sendStatus(400);
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const loginSuccessful = users.find(user => user.login === req.body.login && user.hashed_password === hash);
-        res.send(loginSuccessful);
-    });
+    // if(!req.body) return res.sendStatus(400);
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    //     const loginSuccessful = users.find(user => user.login === req.body.login && user.hashed_password === hash);
+    //     res.send(loginSuccessful);
+    // });
+    db.ba_users.find( (err, tasks) => res.send(tasks));
 });
 
 app.route('/api/cats').get((req, res) => {
